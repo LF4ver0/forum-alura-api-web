92e3f3a0abb5ed21c9d98bd085add6bf41c50fa0
LFavero <lucasfelipe18164@gmail.com>
2025-11-03 22:30:04 -0300

    Implementando Segurança com Spring Security - Aula 1

---
+++ src/main/kotlin/br/com/alura/forum/config/SecurityConfiguration.kt
@@ -0,0 +1,31 @@
+package br.com.alura.forum.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfiguration(
+    private val userDetailsService: UserDetailsService
+){
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .csrf { it.disable() }
+            .authorizeHttpRequests {
+                it.anyRequest().authenticated()
+            }
+            .sessionManagement {
+                it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            }.httpBasic(Customizer.withDefaults()).build()
+    }
+
+    @Bean
+    fun encoder(): PasswordEncoder = BCryptPasswordEncoder()
+}
