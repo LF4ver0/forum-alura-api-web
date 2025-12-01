7eaad5e26902f9718d2ff43003131ed0e08c7734
LFavero <lucasfelipe18164@gmail.com>
2025-12-01 20:44:15 -0300

    Finalizados testes de containers

--- src/test/kotlin/br/com/alura/forum/integration/TopicoRepositoryTest.kt
+++ src/test/kotlin/br/com/alura/forum/integration/TopicoRepositoryTest.kt
@@ -1,11 +1,51 @@
 package br.com.alura.forum.integration
 
+import br.com.alura.forum.dto.TopicoPorCategoriaDto
+import br.com.alura.forum.model.TopicoTest
 import br.com.alura.forum.repository.TopicoRepository
+import org.assertj.core.api.Assertions.assertThat
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.DynamicPropertyRegistrar
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.MySQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import kotlin.test.Test
 
+@Testcontainers
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class TopicoRepositoryTest {
 
     @Autowired
     private lateinit var topicoRepository: TopicoRepository
+    
+    companion object {
+        @Container
+        private val mysqlContainer = MySQLContainer<Nothing>("mysql:8.4.0").apply {
+            withDatabaseName("testedb")
+            withUsername("joao")
+            withPassword("123456")
+        }
 
+        @JvmStatic
+        @DynamicPropertySource
+        fun properties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl)
+            registry.add("spring.datasource.password", mysqlContainer::getPassword)
+            registry.add("spring.datasource.username", mysqlContainer::getUsername)
+        }
+    }
+
+    @Test
+    fun `deve gerar um relatorio`() {
+        val relatorio = topicoRepository.relatorio()
+
+        assertThat(relatorio).isNotNull
+        assertThat(relatorio.first()).isExactlyInstanceOf(TopicoPorCategoriaDto::class.java)
+
+    }
 }
